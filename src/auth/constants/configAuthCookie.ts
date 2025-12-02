f5c1b7f866f27ea0450a31e338b9006ca5d007e4
Braian Palacios <braianpalacios699@gmail.com>
2025-12-02 10:32:48 -0300

    probando cambio de configuracion en cookie parser

--- src/auth/constants/configAuthCookie.ts
+++ src/auth/constants/configAuthCookie.ts
@@ -8,6 +8,6 @@ export const configAuthCookie: CookieOptions = {
   httpOnly: true, //NO PERMITE QUE SE LEA EN NAVEGADOR CLIENTE MEDIANTE document.cookie,
   path: '/', //RUTA PARA ESTA ASIGNACION
   maxAge: ONE_WEEK_IN_MS, //EXPIRACION DE COOKIE
-  sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax', // SI ESTA EN PRODUCCION SERA ESTRICTO SINO LAX
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // SI ESTA EN PRODUCCION SERA ESTRICTO SINO LAX
   secure: process.env.NODE_ENV === 'production', //SEGURIDAD CON HTTPS SI ESTA EN PRODUCCION
 };
